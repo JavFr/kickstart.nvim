144e58431aca5f881efdf77a29c53acf9e78efed
Javier Brustolon <javier.brustolon@umsa.edu.ar>
2023-03-24 16:41:46 -0300

    feat(ui): adding bufferline

---
+++ lua/custom/plugins/ui.lua
@@ -0,0 +1,72 @@
+-- Unless you are still migrating, remove the deprecated commands from v1.x
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+return {
+  {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  keys = {
+      {
+        "<leader>e",
+        function()
+          require("neo-tree.command").execute({ toggle = true })
+        end,
+        desc = "Explorer NeoTree (root dir)",
+      },
+      {
+        "<leader>E",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+  },
+  deactivate = function()
+    vim.cmd([[Neotree close]])
+  end,
+  config = function ()
+    require('neo-tree').setup {}
+  end,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "L", "<Cmd>BufferLineCycleNext<CR>", desc = "Go to next buffer" },
+      { "H", "<Cmd>BufferLineCyclePrev<CR>", desc = "Go to prev buffer" },
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+      { "<leader>bmn", "<Cmd>BufferLineMoveNext<CR>", desc = "Mover buffer to next position" },
+      { "<leader>bmp", "<Cmd>BufferLineMovePrev<CR>", desc = "Mover buffer to prev position" },
+      { "<leader>bsd", "<Cmd>BufferLineSortByDirectory<CR>", desc = "Sort buffers by directory" },
+      { "<leader>bse", "<Cmd>BufferLineSortByExtension<CR>", desc = "Sort buffers by extension" },
+      { "<leader>bsr", "<Cmd>BufferLineSortByRelativeDirectory<CR>", desc = "Sort buffers by relative directory" },
+      { "<leader>bst", "<Cmd>BufferLineSortByTabs<CR>", desc = "Sort buffers by tabs" },
+    },
+    opts = {
+      options = {
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        -- diagnostics_indicator = function(_, _, diag)
+        --   local icons = require("lazyvim.config").icons.diagnostics
+        --   local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+        --     .. (diag.warning and icons.Warn .. diag.warning or "")
+        --   return vim.trim(ret)
+        -- end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+    },
+  },
+}
